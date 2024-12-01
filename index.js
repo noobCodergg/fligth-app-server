8ae66ab53497660cf9c2c66d97a183b3b8e309f1
noobCodergg <113461375+noobCodergg@users.noreply.github.com>
2024-12-01 21:41:31 +0600

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin:'flight-app-frontend-beta.vercel.app',
+    origin:'https://peaceful-tapioca-670ae1.netlify.app/',
     methods:["POST","GET","PUT","DELETE"],
     credentials:true
 }))
